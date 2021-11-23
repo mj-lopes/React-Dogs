c3a53c3ce03537cc009efc2af50080fa59f2835e
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-11-22 21:14:01 -0300

    PhotoContent, Photo, FeedModal refatorados

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { PHOTO_GET } from "../../api";
-import useFetch from "../../Hooks/useFetch";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPhoto } from "../../Store/photo";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import style from "./FeedModal.module.css";
 import PhotoContent from "./Photo/PhotoContent";
 
 function FeedModal({ photo, setModalPhoto }) {
-  const { data, error, loading, request } = useFetch();
+  const { data, error, loading } = useSelector((state) => state.photo);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    const { url, options } = PHOTO_GET(photo.id);
-    request(url, options);
-  }, [photo, request]);
+    dispatch(fetchPhoto(photo.id));
+  }, [photo, dispatch]);
 
   function handleOutsideClick(event) {
     if (event.target === event.currentTarget) setModalPhoto(null);
@@ -22,7 +22,7 @@ function FeedModal({ photo, setModalPhoto }) {
     <div className={style.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
-      {data && <PhotoContent data={data} />}
+      {data && <PhotoContent />}
     </div>
   );
 }
